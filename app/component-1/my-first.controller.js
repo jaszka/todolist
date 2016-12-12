6c2285d240b22b0d39f08ba2bca1d0c0306ed57f
jachowicz <aleksandra.jachowicz@capgemini.com>
2016-12-12 17:58:10 +0100

    adding and editing tasks

--- app/component-1/my-first.controller.js
+++ app/component-1/my-first.controller.js
@@ -1,73 +1,73 @@
-angular.module('app.component1').controller('MyFirstController', function ($scope, $http, $modal, todoService) {
+angular.module('app.component1').controller('FirstViewController', function ($scope, $modal, $filter, $window, todoService, todos) {
     'use strict';
 
-    $scope.data = {
-        helloWorld: 'hello world',
-        form: {
-            category: '',
-            title: '',
-            priority: '',
-            content: '',
-            date: ''
-        }
-    }
+    $scope.todos = todos;
+    $scope.todo = {};
 
-    $scope.filter = 'category';
+    $scope.$on("updateView", function () {
+        $window.location.reload();
+    });
+
+    $scope.filter = 'title';
     $scope.search = {
         category: '',
         title: ''
     };
 
-    $scope.edit = function (index) {
+    $scope.orderByCreationTime = function () {
+        console.log($scope.todos);
+        return $scope.todos = $filter('orderBy')($scope.todos, $scope.todo.addetAt, true);
+    };
+
+    $scope.view = function (id) {
         $modal.open({
             templateUrl: '/component-1/modal-dialog/modal-dialog.tpl.html',
-            controller: 'MyModalController',
+            controller: 'ViewTaskController',
             size: 'lg',
             backdrop: 'static',
             keyboard: false,
             resolve: {
                 selectedTodo: function () {
-                    return $scope.todos[index];
+                    return $scope.todo = $filter('filter')(todos, function (todo) { return todo.id === id; })[0];
+                },
+                todos: function () {
+                    return $scope.todos;
                 }
             }
-        });
+        })
     };
 
     //$scope.test = console.log(todoService.todosTest());
 
-    todoService.todosList().then(function (list) {
-        $scope.todos = list;
-    });
+    // todoService.todosList().then(function (list) {
+    //     $scope.todos = list;
+    // });
 
-    $scope.add = function () { //TODO change to add todo form
+    $scope.add = function () {
         $modal.open({
             templateUrl: '/component-1/modal-dialog/modal-add.tpl.html',
-            controller: 'MyModalController',
-            size: 'lg',
+            controller: 'AddTaskController',
+            size: 'md',
             backdrop: 'static',
-            keyboard: false,
-            resolve: {
-                selectedTodo: function () {
-                    return $scope.data.form;
-                }
-            }
+            keyboard: false
         });
     };
 
-    $scope.print = function () {
-        todoService.print();
-    }
+    $scope.totalItems = $scope.todos.length;
+    $scope.filteredTodos = [];
+    $scope.currentPage = 1;
+    $scope.numPerPage = 5;
+    $scope.noOfPages = Math.ceil($scope.totalItems / $scope.numPerPage);
 
-    $http.get('http://rest-service.guides.spring.io/greeting').
-        then(function (response) {
-            $scope.greeting = response.data;
-        });
+    $scope.$watch('currentPage + numPerPage', function () {
+        var begin = (($scope.currentPage - 1) * $scope.numPerPage)
+            , end = begin + $scope.numPerPage;
+        $scope.filteredTodos = $scope.todos.slice(begin, end);
+    }, true);
 
     //controllery sa funkcja konstruktowa
 
-
-
-}).controller('MyModalController', function ($scope, $modalInstance, selectedTodo) {
+}).controller('ViewTaskController', function ($scope, $modal, $filter, $modalInstance, selectedTodo, todos) {
     'use strict';
 
     $scope.selectedTodo = selectedTodo;
@@ -76,18 +76,64 @@ angular.module('app.component1').controller('MyFirstController', function ($scop
         $modalInstance.close();
     };
 
-    $scope.categories = ["MEETING", "SHOPPING", "REMINDER"];
-
-    $scope.priorities = ["HIGH", "MEDIUM", "LOW"];
-
-    $scope.submitForm = function (isValid) {
-        if (isValid) {
-            alert('our form is amazing');
-        }
+    $scope.edit = function (id) {
+        $modalInstance.close();
+        $modal.open({
+            templateUrl: '/component-1/modal-dialog/modal-edit.tpl.html',
+            controller: 'EditTaskController',
+            size: 'md',
+            backdrop: 'static',
+            keyboard: false,
+            resolve: {
+                selectedTodo: function () {
+                    return $scope.selectedTodo;
+                }
+            }
+        });
     };
 
-    $scope.doSomethingWithDate = function (date) {
-        alert(date);
+}).controller('AddTaskController', function ($scope, $rootScope, $modalInstance, todoService) {
+
+    $scope.categories = ["MEETING", "SHOPPING", "REMINDER"];
+    $scope.priorities = ["HIGH", "MEDIUM", "LOW"];
+
+    $scope.submitForm = function () {
+        todoToAdd = $scope.todo;
+        addedTodo = todoService.addTodo(todoToAdd);
+        $modalInstance.close();
+        $rootScope.$broadcast('updateView');
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.close();
+    };
+
+    $(function () {
+        $('#datetimepicker').datetimepicker({
+            useStrict: true,
+            format: 'YYYY-MM-DD HH:mm',
+            stepping: 10,
+            allowInputToggle: true
+        });
+    });
+
+}).controller('EditTaskController', function ($scope, $rootScope, $modalInstance, todoService, selectedTodo) {
+
+    $scope.categories = ["MEETING", "SHOPPING", "REMINDER"];
+    $scope.priorities = ["HIGH", "MEDIUM", "LOW"];
+
+    $scope.editedTodo = {};
+    angular.copy(selectedTodo, $scope.editedTodo);
+    $scope.taskTitle = $scope.editedTodo.title;
+
+    $scope.submitForm = function () {
+        changedTodo = todoService.amendTodo($scope.editedTodo);
+        $modalInstance.close();
+        $rootScope.$broadcast('updateView');
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.close();
     };
 
 })
