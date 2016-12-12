33dcd0669c20e2561cc9edc361bb658d1fbae81b
jachowicz <aleksandra.jachowicz@capgemini.com>
2016-12-12 17:56:39 +0100

    names changes

--- app/component-1/component-1.module.js
+++ app/component-1/component-1.module.js
@@ -3,11 +3,10 @@ angular.module('app.component1', ['ngRoute', 'app.component1.templates'])
         'use strict';
         $routeProvider.when('/component-1/dialog-a', {
             templateUrl: 'component-1/dialog-a/dialog-a.html',
-            controller: 'MyFirstController',
-            controllerAs: 'myFirstController',
+            controller: 'FirstViewController',
             resolve: {
-                books: function ($http) {
-                    return $http.get('/component-1/books.json');
+                todos: function (todoService) {
+                    return todoService.todosList();
                 }
             }
         });
