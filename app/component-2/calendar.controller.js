1b3478f03cc7db0d28c75aa2cd59fef615a25c3c
jachowicz <aleksandra.jachowicz@capgemini.com>
2016-12-14 14:35:42 +0100

    chaging status on calendar view

--- app/component-2/calendar.controller.js
+++ app/component-2/calendar.controller.js
@@ -1,23 +1,39 @@
-angular.module('app.component2').controller('SecondViewController', function (uiCalendarConfig, $scope, $modal, $filter, $window, todoService, todos) {
+angular.module('app.component2').controller('SecondViewController', function(uiCalendarConfig, $scope, $modal, $filter, $window, todoService, todos) {
     'use strict';
 
     $scope.events = [];
     $scope.eventSources = [$scope.events];
-    //angular.copy(todos, $scope.eventSources);
 
     $scope.selectedEvent = null;
     var isFirstTime = true;
 
-    $scope.events.slice(0, $scope.events.length);
+    var setColor = function(priority) {
+        if (priority === "HIGH") {
+            return "#cc0000"
+        }
+        if (priority === "MEDIUM") {
+            return "#ffb400"
+        }
+        if (priority === "LOW") {
+            return "#56e25f"
+        }
+    }
 
+
+    $scope.events.slice(0, $scope.events.length);
     angular.forEach(todos, function(todo) {
         $scope.events.push({
+            id: todo.id,
             title: todo.title,
             category: todo.category,
-            description: todo.description,
-            start: new Date(todo.date.year, todo.date.monthValue - 1, todo.date.dayOfMonth),
-            end: new Date(todo.date.year, todo.date.monthValue - 1, todo.date.dayOfMonth),
-            stick: true
+            content: todo.content,
+            priority: todo.priority,
+            start: new Date(todo.date),
+            end: new Date(todo.date) + 36000,
+            status: todo.status,
+            allDayDefault: true,
+            stick: true,
+            color: setColor(todo.priority)
         })
     });
 
@@ -26,21 +42,39 @@ angular.module('app.component2').controller('SecondViewController', function (ui
             height: 500,
             editable: true,
             displayEventTime: false,
+            firstDay: 1,
             header: {
                 left: 'month agendaWeek agendaDay',
                 center: 'title',
                 right: 'today prev,next'
             },
-            eventClick: function (event) {
+            eventMouseOver: function(event) {
+                alert("hi");
+            },
+            eventMouseOut: function(event) {
+                alert("bye");
+            },
+            eventClick: function(event) {
                 $scope.selectedEvent = event;
-            },
-            eventAfterAllRender: function () {
-                if ($scope.events.length > 0 && isFirstTime) {
-                    uiCalendarConfig.calendars.myCalendar.fullCalendar('gotoDate', $scope.events[0].start);
-                    isFirstTime = false;
-                }
-
-            },
+                $modal.open({
+                    templateUrl: '/main/layout/modal-dialog.tpl.html',
+                    controller: 'TodoViewController',
+                    size: 'md',
+                    backdrop: 'static',
+                    keyboard: true,
+                    resolve: {
+                        selectedTodo: function() {
+                            return $scope.selectedEvent;
+                        },
+                        list: function() {
+                            return false;
+                        },
+                        calendar: function() {
+                            return true;
+                        }
+                    }
+                });
+            }
             //eventDrop: $scope.alertOnDrop,
             //eventResize: $scope.alertOnResize
         }
--- app/main/service/todo.service.js
+++ app/main/service/todo.service.js
@@ -12,4 +12,8 @@ angular.module('app').service('todoService', function($http) {
         return $http.post('http://localhost:8090/todos?update', todo).then(function(response) { return response.data });
     }
 
+    this.testTodo = function(todo) {
+        return $http.post('http://localhost:8090/todos?update', todo).then(function(response) { return response.data.status }, function(response) {alert(response.data.message) });
+    }
+
 });
